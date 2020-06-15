a14ac92fb5ddc85ac2b505755340067d20ee7b5d
Jonas Benner <bennerjonas10@gmail.com>
2020-06-15 15:32:47 +1000

    Sales price IQR

--- Preprocessed_Housing_Data.py
+++ Preprocessed_Housing_Data.py
@@ -1,5 +1,29 @@
 import pandas as pd
+import numpy as np
 
+# Set the option of displaying all columns (this will be a lot but good for scanning over the information)
+pd.set_option('display.max_columns', None)
 
-train = pd.read_csv('/Users/Jonas/Desktop/DataScience/Kaggle/House\\ Prices/CSVs/train.csv')
-print(train.head())
+# Import csv file
+train = pd.read_csv('/Users/Jonas/Desktop/DataScience/Kaggle/HousePrices/CSVs/train.csv')
+
+# Inspect general information and get some statistics
+# print(train.info())
+# print(train.describe())
+
+# Get a sense of the target value
+# print(train.SalePrice.describe())
+# Median sales price is 163k and IQR is between 129k to 214k
+
+# Save the first and third quartile as lower quartile and upper quartile
+lqr = np.percentile(train.SalePrice, 25)
+uqr = np.percentile(train.SalePrice, 75)
+
+# Now it's about inspecting some of the features to see if any seem more relevant than others
+# First feature to inspect is the neighborhood
+# print(train.groupby('Neighborhood').SalePrice.mean())
+# There seem to be big differences in sales prices across neighborhoods
+
+# Separate the neighborhoods into three groups: below IQR, within IQR and above IQR
+print(train.isna().any())
+
